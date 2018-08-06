576fdbc1a3886e3972acc66be68414ffc3d26828
AKa - Shinobi <alif_lk@yahoo.com>
2018-08-06 21:07:24 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -86,7 +86,7 @@ class LineAPI {
   _qrCodeLogin() {
     this.setTHttpClient();
     return new Promise((resolve, reject) => {
-    this._client.getAuthQrcode(true, 'Alfathdirk-PC',(err, result) => {
+    this._client.getAuthQrcode(true, 'Shinobi-PC',(err, result) => {
       const qrcodeUrl = `line://au/q/${result.verifier}`;
       qrcode.generate(qrcodeUrl,{small: true});
       console.info(`\n\nlink qr code is: ${qrcodeUrl}`)
@@ -134,7 +134,7 @@ class LineAPI {
          reqx.password = rsaCrypto.credentials;
          reqx.keepLoggedIn = true;
          reqx.accessLocation = this.config.ip;
-         reqx.systemName = 'LineAlphatFork';
+         reqx.systemName = 'LineShinobi';
          reqx.e2eeVersion = 0;
          try{
            this._client.loginZ(reqx,
